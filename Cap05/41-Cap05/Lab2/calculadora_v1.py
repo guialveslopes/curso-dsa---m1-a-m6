8ecb0e54ff3afc4dfecaade56376c9f88a052efa
Guilherme Alves <alvesguilherme2323@gmail.com>
2024-07-09 08:20:47 -0300

    Repósitorio
    Neste repositório estão os arquivos do modulo 2 ao 6 do curso de analise e ciência de dados com Python da DSA que acabei de finalizar. 
    02 - Visão geral
    03 - introdução a logica de programação
    04 - variáveis, tipos e estruturas de dados
    05 - Loops, condicionais, métodos e funções
    06 - Tratamento de arquivos, módulos, pacotes e funções built-in

---
+++ Cap05/41-Cap05/Lab2/calculadora_v1.py
@@ -0,0 +1,13 @@
+# Calculadora em Python
+
+# Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos até aqui no curso. 
+# A solução será apresentada no próximo capítulo!
+print('1 - Soma\n2 - Subtrção\n3 - Multiplicação\n4 - Divisão')
+opcao = input('Digite sua opção(1/2/3/4): ')
+
+
+
+
+print("\n******************* Calculadora em Python *******************")
+
+
